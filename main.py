15b9a27738ea758cb2778466800bdaa6074e6b44
Lucas Isla <lucasi2@illinois.edu>
2020-05-11 13:34:38 -0500

    Plot all 4 graphs side-by-side

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ f_cutoff = 2
 f_cutoff_ratio = f_cutoff / f_nyquist
 order = 2
 delta_t = 0.01
-acc_filename = "./data_imu_loc/route1/Accelerometer.csv"
-gyro_filename = "./data_imu_loc/route1/Gyroscope.csv"
 
 def read_data(file_name):
     data = []
@@ -90,10 +88,12 @@ def get_gyro_readings_between_times(first_step_time, second_step_time):
 def axis_angle_to_matrix(axis, theta):
     normalized_axis = axis / la.norm(axis)
     [ux, uy, uz] = normalized_axis
+    c = np.cos(theta)
+    s = np.sin(theta)
     # Source: https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-    R = np.array([[np.cos(theta)+((ux**2)*(1-np.cos(theta))), (ux*uy*(1-np.cos(theta)))-(uz*np.sin(theta)), (ux*uz*(1-np.cos(theta)))+(uy*np.sin(theta))],
-                  [(ux*uy*(1-np.cos(theta)))+(uz*np.sin(theta)), np.cos(theta)+((uy**2)*(1-np.cos(theta))), (uy*uz*(1-np.cos(theta)))-(ux*np.sin(theta))],
-                  [(ux*uz*(1-np.cos(theta)))-(uy*np.sin(theta)), (uy*uz*(1-np.cos(theta)))+(ux*np.sin(theta)), np.cos(theta)+((uz**2)*(1-np.cos(theta)))]])
+    R = np.array([[c+((ux**2)*(1-c)),    (ux*uy*(1-c))-(uz*s), (ux*uz*(1-c))+(uy*s)],
+                  [(ux*uy*(1-c))+(uz*s), c+((uy**2)*(1-c)),    (uy*uz*(1-c))-(ux*s)],
+                  [(ux*uz*(1-c))-(uy*s), (uy*uz*(1-c))+(ux*s), c+((uz**2)*(1-c))]])
     return R
 
 # Integrate gyro data (between 2 steps),
@@ -103,8 +103,8 @@ def integrate_gyros(gyro_data, R_init):
     delta_Ri = R_init
     for l in gyro_data:
         delta_theta = np.sqrt(l[0]**2 + l[1]**2 + l[2]**2) * delta_t
-        # For now, DON'T project instant rotation axis l into global frame
-        proj_l = l
+        # Project instant rotation axis l into global frame (?)
+        proj_l = R0 @ l
         new_delta_Ri = axis_angle_to_matrix(proj_l, delta_theta)
         # R(i+1) = delta_Ri * Ri
         delta_Ri = delta_Ri @ new_delta_Ri
@@ -123,64 +123,99 @@ def matrix_to_axis_angle(A):
 def get_step_length_from_angle(angle):
     return 0.762*angle
 
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
 
-# ---- Read in data ----
-acc_data = read_data(acc_filename)
-gyro_data = read_data(gyro_filename)
+# Source: https://www.kite.com/python/answers/how-to-rotate-a-3d-vector-about-an-axis-in-python
+def get_perpendicular(v):
+    # Rotate v around [0,0,-1] by 90 degrees
+    rot_radians = np.radians(90)
+    rot_axis = np.array([0, 0, 1])
 
-# # Change ms to s?
-# acc_data = [[row[0]/1000.0, row[1], row[2], row[3]] for row in acc_data]
-# gyro_data = [[row[0]/1000.0, row[1], row[2], row[3]] for row in gyro_data]
+    rot_vector = rot_radians * rot_axis
+    rotation = R.from_rotvec(rot_vector)
+    rotated_v = rotation.apply(v)
+    return rotated_v
 
-init_acc_data = acc_data[0][1:]
-R0 = get_init_orientation(init_acc_data)
-acc_times = [row[0] for row in acc_data]
-gyro_times = [row[0] for row in gyro_data]
+def project_onto_hor(v):
+    grav = np.array([0,0,-1])
+    proj_onto_grav = (np.dot(v, grav)/(la.norm(grav))) * grav
+    proj_onto_hor = v - proj_onto_grav
+    proj_onto_hor = proj_onto_hor / la.norm(proj_onto_hor)
+    return proj_onto_hor
 
-# Get acc mags, subtract out average mag from them
-acc_data_mags = [magnitude(row) for row in acc_data]
-mag_avg = np.mean(acc_data_mags)
-acc_data_mags = [(mag - mag_avg) for mag in acc_data_mags]
+# Plotting all 4 routes side-by-side
 
-# ---- Filter & count steps from acc_data----
-filtered_data = low_pass_filter(acc_data_mags)
-(step_indices, props) = find_peaks(filtered_data, threshold=0.0001)
-peaks = [filtered_data[i] for i in step_indices]
-num_steps = len(peaks)
-step_times = [acc_data[i][0] for i in step_indices]
+plt.figure()
+plot_index = 221
+for route in ['1', '2', '3', '4']:
 
-# # Find which of the leg's steps (even / odd) has greater average mag
-# # This is the pocket leg
-# (pl_step_indices, non_pl_step_indices, pl_peaks, non_pl_peaks) = get_pocket_leg_info(step_indices, filtered_data)
-# plot_range(pl_step_indices, filtered_data)
+    acc_filename = './data_imu_loc/route' + route + '/Accelerometer.csv'
+    gyro_filename = './data_imu_loc/route' + route + '/Gyroscope.csv'
 
 
-# ---- Track walking direction & next locations step-wise ----
-# Start at origin
-locs = []
-locs.append(np.array([0,0,0]))
-# First delta R is the initial orientation, since successive delta_R's build off of that
-delta_R = R0
-all_gyros = []
-# For each 2 successive steps for the leg, ...
-for i in range(len(step_times) - 1):
-    # Get all gyro readings between those 2 steps
-    gyros = get_gyro_readings_between_times(step_times[i], step_times[i+1])
-    # Take just x,y,z of gyro readings
-    gyros = [gyro[1:] for gyro in gyros]
-    # Integrate those gyro readings and multiply them w/ orientation
-    delta_R = integrate_gyros(gyros, delta_R)
-    # Express that as axis-angle
-    (axis,angle) = matrix_to_axis_angle(delta_R)
-    # Get orthogonal vector -> this is walking direction (for now I'm just gonna use the axis)
-    walking_dir = np.array(axis)
-    step_length = get_step_length_from_angle(angle)
-    # Multiply that walking direction w/ step length
-    disp_of_step = step_length * walking_dir
-    locs.append(locs[i] + disp_of_step)
+    # ---- Read in data ----
+    acc_data = read_data(acc_filename)
+    gyro_data = read_data(gyro_filename)
+    # Round times to nearest 0.01 s (regard as constant 100Hz)
+    acc_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in acc_data]
+    gyro_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in gyro_data]
 
-locs = np.array(locs)
-plt.plot(locs[:,0], locs[:,1])
+
+    init_acc_data = acc_data[0][1:]
+    R0 = get_init_orientation(init_acc_data)
+    grav_dir = init_acc_data / la.norm(init_acc_data)
+    acc_times = [row[0] for row in acc_data]
+    gyro_times = [row[0] for row in gyro_data]
+
+    # Get acc mags, subtract out average mag from them
+    acc_data_mags = [magnitude(row) for row in acc_data]
+    mag_avg = np.mean(acc_data_mags)
+    acc_data_mags = [(mag - mag_avg) for mag in acc_data_mags]
+
+    # ---- Filter & count steps from acc_data----
+    filtered_data = low_pass_filter(acc_data_mags)
+    (step_indices, props) = find_peaks(filtered_data, threshold=0.0001)
+    peaks = [filtered_data[i] for i in step_indices]
+    num_steps = len(peaks)
+    step_times = [acc_data[i][0] for i in step_indices]
+
+    # # Find which of the leg's steps (even / odd) has greater average mag
+    # # This is the pocket leg
+    # (pl_step_indices, non_pl_step_indices, pl_peaks, non_pl_peaks) = get_pocket_leg_info(step_indices, filtered_data)
+    # pl_step_times = [acc_data[i][0] for i in pl_step_indices]
+    # plot_range(pl_step_indices, filtered_data)
+
+
+    # ---- Track walking direction & next locations step-wise ----
+    # Start at origin
+    locs = []
+    locs.append(np.array([0,0,0]))
+    # First delta R is the initial orientation, since successive delta_R's build off of that
+    delta_R = R0
+    # For each 2 successive steps for the leg, ...
+    for i in range(len(step_times) - 1):
+        gyros = get_gyro_readings_between_times(step_times[i], step_times[i+1])
+        gyros = [gyro[1:] for gyro in gyros]
+        delta_R = integrate_gyros(gyros, delta_R)
+        (axis,angle) = matrix_to_axis_angle(delta_R)
+        # Express axis in global frame, project onto hor. plane, then find perp. vector?
+        axis = R0 @ axis
+        axis = project_onto_hor(axis)
+        # For now, just don't get perp. since it only rotates the plot
+        # walking_dir = get_perpendicular(axis)
+        walking_dir = axis
+        step_length = get_step_length_from_angle(angle)
+        disp_of_step = step_length * walking_dir
+        locs.append(locs[i] + disp_of_step)
+
+    # Plot all 4 routes side-by-side (as 4 subplots)
+
+    locs = np.array(locs)
+    plt.axis('equal')
+    plt.subplot(plot_index)
+    plt.plot(locs[:,0], locs[:,1])
+    plot_index += 1
+
 plt.show()
-
-print(num_steps)
