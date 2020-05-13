7e9183e23530e1b0475b5bf75172b24ae91583ab
Lucas Isla <lucasi2@illinois.edu>
2020-05-12 22:16:12 -0500

    Get closer results for routes 2 + 4

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ f_cutoff_ratio = f_cutoff / f_nyquist
 order = 2
 delta_t = 0.01
 
+peak_threshold=0.0001
+peak_prominence=1.0
+
 def read_data(file_name):
     data = []
     with open(file_name) as csv_file:
@@ -29,6 +32,10 @@ def read_data(file_name):
 def low_pass_filter(data):
     # f_cutoff_ratio gives ratio of the <desired frequency = 2Hz> : <Nyquist frequency = 0.5*100Hz>
     b, a = butter(order, f_cutoff_ratio, btype='low', analog=False)
+    return filtfilt(b, a, data)
+
+def high_pass_filter(data):
+    b, a = butter(order, f_cutoff_ratio, btype='high', analog=False)
     return filtfilt(b, a, data)
 
 # Get magnitude of row by using Pythagorean on x,y,z (1,2,3) IMU data
@@ -83,7 +90,13 @@ def get_init_orientation(acc_unnormalized):
     return R
 
 def get_gyro_readings_between_times(first_step_time, second_step_time):
-    return filter(lambda row: row[0] >= first_step_time and row[0] <= second_step_time, gyro_data)
+    gyros = list(filter(lambda row: row[0] > first_step_time and row[0] <= second_step_time, gyro_data))
+    return [gyro[1:] for gyro in gyros]
+
+def get_gyro_delta_Rs_between_times(first_step_time, second_step_time, all_delta_Rs):
+    start_index = ts_to_index(first_step_time)
+    end_index = ts_to_index(second_step_time)
+    return all_delta_Rs[start_index : end_index]
 
 def axis_angle_to_matrix(axis, theta):
     normalized_axis = axis / la.norm(axis)
@@ -96,19 +109,37 @@ def axis_angle_to_matrix(axis, theta):
                   [(ux*uz*(1-c))-(uy*s), (uy*uz*(1-c))+(ux*s), c+((uz**2)*(1-c))]])
     return R
 
-# Integrate gyro data (between 2 steps),
-# multiplying each new delta_R together, starting w/ an initial R
-# The initial R (R_init) is the orientation from the previous step's iteration
-def integrate_gyros(gyro_data, R_init):
-    delta_Ri = R_init
+# Integrate gyro data (within a step)
+def integrate_gyros(gyro_data, R):
+    delta_R = R
+    # Looking at gyro data for each timestamp within the step
     for l in gyro_data:
         delta_theta = np.sqrt(l[0]**2 + l[1]**2 + l[2]**2) * delta_t
-        # Project instant rotation axis l into global frame (?)
-        proj_l = R0 @ l
-        new_delta_Ri = axis_angle_to_matrix(proj_l, delta_theta)
-        # R(i+1) = delta_Ri * Ri
-        delta_Ri = delta_Ri @ new_delta_Ri
-    return delta_Ri
+        l_proj = delta_R @ l
+        delta_R_gyro = axis_angle_to_matrix(l_proj, delta_theta)
+        delta_R = delta_R_gyro @ delta_R
+    return delta_R
+
+# Multiply all gyro_delta_Rs together and return
+def integrate_gyro_delta_Rs(gyro_delta_Rs):
+    delta_R_for_step = np.eye(3)
+    for gyro_delta_R in gyro_delta_Rs:
+        delta_R_for_step = gyro_delta_R @ delta_R_for_step
+    return delta_R_for_step
+
+# Integrate all gyro readings, put these matrices in a (massive) array, return
+def integrate_all_gyros(gyro_data, R):
+    Ri = R0
+    all_Rs = []
+    all_delta_Rs = []
+    for l in gyro_data:
+        delta_theta = np.sqrt(l[0]**2 + l[1]**2 + l[2]**2) * delta_t
+        proj_l = Ri @ l
+        delta_Ri = axis_angle_to_matrix(proj_l, delta_theta)
+        Ri = delta_Ri @ Ri
+        all_Rs.append(Ri)
+        all_delta_Rs.append(delta_Ri)
+    return all_Rs, all_delta_Rs
 
 # Convert rotation matrix to axis & angle
 def matrix_to_axis_angle(A):
@@ -145,29 +176,52 @@ def project_onto_hor(v):
     proj_onto_hor = proj_onto_hor / la.norm(proj_onto_hor)
     return proj_onto_hor
 
+def get_acc_data_global(acc_data, all_Rs, R0):
+    acc_data_global = []
+    for i in range(len(all_Rs)):
+        time = acc_data[i][0]
+        current_R = all_Rs[i]
+        current_acc = np.array(acc_data[i][1:])
+        current_acc_g = current_R @ current_acc
+        acc_data_global.append([time, current_acc_g[0], current_acc_g[1], current_acc_g[2]])
+    return acc_data_global
+
+def ts_to_index(timestamp):
+    return int(timestamp/10)
+def index_to_ts(index):
+    return float(index*10)
+
 # Plotting all 4 routes side-by-side
 
-plt.figure()
+plt.figure(figsize=(16,9))
 plot_index = 221
 for route in ['1', '2', '3', '4']:
 
     acc_filename = './data_imu_loc/route' + route + '/Accelerometer.csv'
     gyro_filename = './data_imu_loc/route' + route + '/Gyroscope.csv'
 
-
     # ---- Read in data ----
     acc_data = read_data(acc_filename)
     gyro_data = read_data(gyro_filename)
+
+    # Trim down data so they are the same # samples
+    min_length = min(len(acc_data), len(gyro_data))
+    acc_data = acc_data[:min_length]
+    gyro_data = gyro_data[:min_length]
+
     # Round times to nearest 0.01 s (regard as constant 100Hz)
     acc_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in acc_data]
     gyro_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in gyro_data]
-
+    acc_times = [row[0] for row in acc_data]
+    gyro_times = [row[0] for row in gyro_data]
 
     init_acc_data = acc_data[0][1:]
     R0 = get_init_orientation(init_acc_data)
-    grav_dir = init_acc_data / la.norm(init_acc_data)
-    acc_times = [row[0] for row in acc_data]
-    gyro_times = [row[0] for row in gyro_data]
+
+    gyro_data_no_times = [row[1:] for row in gyro_data]
+    all_Rs, all_delta_Rs = integrate_all_gyros(gyro_data_no_times, R0)
+    acc_data_global = get_acc_data_global(acc_data, all_Rs, R0)
+    acc_data_global_zs = [row[3] for row in acc_data_global]
 
     # Get acc mags, subtract out average mag from them
     acc_data_mags = [magnitude(row) for row in acc_data]
@@ -176,9 +230,10 @@ for route in ['1', '2', '3', '4']:
 
     # ---- Filter & count steps from acc_data----
     filtered_data = low_pass_filter(acc_data_mags)
-    (step_indices, props) = find_peaks(filtered_data, threshold=0.0001)
+    (step_indices, props) = find_peaks(filtered_data, threshold=peak_threshold, prominence=peak_prominence)
     peaks = [filtered_data[i] for i in step_indices]
     num_steps = len(peaks)
+    print(num_steps)
     step_times = [acc_data[i][0] for i in step_indices]
 
     # # Find which of the leg's steps (even / odd) has greater average mag
@@ -190,25 +245,31 @@ for route in ['1', '2', '3', '4']:
 
     # ---- Track walking direction & next locations step-wise ----
     # Start at origin
+
     locs = []
     locs.append(np.array([0,0,0]))
     # First delta R is the initial orientation, since successive delta_R's build off of that
-    delta_R = R0
+    delta_R_for_step = R0
     # For each 2 successive steps for the leg, ...
+
+    even = True
+
     for i in range(len(step_times) - 1):
-        gyros = get_gyro_readings_between_times(step_times[i], step_times[i+1])
-        gyros = [gyro[1:] for gyro in gyros]
-        delta_R = integrate_gyros(gyros, delta_R)
-        (axis,angle) = matrix_to_axis_angle(delta_R)
-        # Express axis in global frame, project onto hor. plane, then find perp. vector?
-        axis = R0 @ axis
+        gyro_delta_Rs = get_gyro_delta_Rs_between_times(step_times[i], step_times[i+1], all_delta_Rs)
+        delta_R_for_step = integrate_gyro_delta_Rs(gyro_delta_Rs)
+
+        (axis,angle) = matrix_to_axis_angle(delta_R_for_step)
         axis = project_onto_hor(axis)
-        # For now, just don't get perp. since it only rotates the plot
-        # walking_dir = get_perpendicular(axis)
+        if not(even):
+            axis = np.array([-1*axis[0], -1*axis[1], axis[2]])
+        axis = get_perpendicular(axis)
+
         walking_dir = axis
         step_length = get_step_length_from_angle(angle)
         disp_of_step = step_length * walking_dir
         locs.append(locs[i] + disp_of_step)
+
+        even = not(even)
 
     # Plot all 4 routes side-by-side (as 4 subplots)
 
@@ -216,6 +277,7 @@ for route in ['1', '2', '3', '4']:
     plt.axis('equal')
     plt.subplot(plot_index)
     plt.plot(locs[:,0], locs[:,1])
+    plt.title('Route ' + route)
     plot_index += 1
 
 plt.show()
