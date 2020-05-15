c18c3aa533d9bf4f02b0f098ee4d340eff558a20
Lucas Isla <lucasi2@illinois.edu>
2020-05-15 10:57:19 -0500

    Add comments, change plotting, account for testing and training data

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ from scipy.spatial.transform import Rotation as R
 from scipy.signal import butter,filtfilt, find_peaks, lfilter
 
 PLOT_ROUTES = True
+PLOT_ROUTES_SAME = True
+PLOT_COLORS = ['blue', 'orange', 'green', 'red', 'cyan', 'magenta', 'black']
+TESTING = False
 
 # Sampling frequency
 f_sampling = 100
 # Nyquist frequency
 f_nyquist = 0.5 * f_sampling
 # Cutoff frequency we want for low-pass filter
-f_cutoff = 2.5
-f_cutoff_ratio = f_cutoff / f_nyquist
+f_cutoff_low = 2.4
+f_cutoff_low_ratio = f_cutoff_low / f_nyquist
+# Cutoff frequency we want for high-pass filter
+f_cutoff_high = 2
+f_cutoff_high_ratio = f_cutoff_high / f_nyquist
 order = 5
 delta_t = 0.01
 
@@ -35,32 +41,17 @@ def read_data(file_name):
 # Uses scipy.butter to apply low-pass filter
 def low_pass_filter(data):
     # f_cutoff_ratio gives ratio of the <desired frequency = 2Hz> : <Nyquist frequency = 0.5*100Hz>
-    b, a = butter(order, f_cutoff_ratio, btype='low', analog=False)
+    b, a = butter(order, f_cutoff_low_ratio, btype='lowpass', analog=False)
     return filtfilt(b, a, data)
-
+# Uses scipy.butter to apply high-pass filter
 def high_pass_filter(data):
-    b, a = butter(order, f_cutoff_ratio, btype='high', analog=False)
+    # f_cutoff_ratio gives ratio of the <desired frequency = 2Hz> : <Nyquist frequency = 0.5*100Hz>
+    b, a = butter(order, f_cutoff_high_ratio, btype='highpass', analog=False)
     return filtfilt(b, a, data)
 
 # Get magnitude of row by using Pythagorean on x,y,z (1,2,3) IMU data
 def magnitude(row):
     return ( (row[1]**2) + (row[2]**2) + (row[3]**2) )**(0.5)
-
-# Find out which leg has the stronger steps by looking at even / odd peaks
-def get_pocket_leg_info(step_indices, filtered_data):
-    even_step_indices = step_indices[0::2]
-    even_peaks = [filtered_data[i] for i in even_step_indices]
-    odd_step_indices = step_indices[1::2]
-    odd_peaks = [filtered_data[i] for i in odd_step_indices]
-
-    pl_step_indices, non_pl_step_indices = even_step_indices, odd_step_indices
-    pl_peaks, non_pl_peaks = even_peaks, odd_peaks
-    if np.mean(odd_peaks) > np.mean(even_peaks):
-        pl_step_indices, non_pl_step_indices = odd_step_indices, even_step_indices
-        pl_peaks, non_pl_peaks = odd_peaks, even_peaks
-
-    return (pl_step_indices, non_pl_step_indices, pl_peaks, non_pl_peaks)
-
 
 # Using accelerometer data (first row of CSV), get initial orientation
 # (assumes projection of phone’s local Y axis onto hor. plane is towards the global Y axis)
@@ -81,15 +72,18 @@ def get_init_orientation(acc_unnormalized):
     R = np.array([row1, v1, v2])
     return R
 
-def get_gyro_readings_between_times(first_step_time, second_step_time):
-    gyros = list(filter(lambda row: row[0] > first_step_time and row[0] <= second_step_time, gyro_data))
+# Get all gyro readings between time_1 and time_2
+def get_gyro_readings_between_times(time_1, time_2):
+    gyros = list(filter(lambda row: row[0] > time_1 and row[0] <= time_2, gyro_data))
     return [gyro[1:] for gyro in gyros]
 
-def get_gyro_delta_Rs_between_times(first_step_time, second_step_time, all_delta_Rs):
-    start_index = ts_to_index(first_step_time)
-    end_index = ts_to_index(second_step_time)
+# Get all delta_R_gyro readings (from large array), between time_1 and time_2
+def get_gyro_delta_Rs_between_times(time_1, time_2, all_delta_Rs):
+    start_index = ts_to_index(time_1)
+    end_index = ts_to_index(time_2)
     return all_delta_Rs[start_index : end_index]
 
+# Convert axis + angle to rotation matrix form
 def axis_angle_to_matrix(axis, theta):
     normalized_axis = axis / la.norm(axis)
     [ux, uy, uz] = normalized_axis
@@ -100,17 +94,6 @@ def axis_angle_to_matrix(axis, theta):
                   [(ux*uy*(1-c))+(uz*s), c+((uy**2)*(1-c)),    (uy*uz*(1-c))-(ux*s)],
                   [(ux*uz*(1-c))-(uy*s), (uy*uz*(1-c))+(ux*s), c+((uz**2)*(1-c))]])
     return R
-
-# Integrate gyro data (within a step)
-def integrate_gyros(gyro_data, R):
-    delta_R = R
-    # Looking at gyro data for each timestamp within the step
-    for l in gyro_data:
-        delta_theta = np.sqrt(l[0]**2 + l[1]**2 + l[2]**2) * delta_t
-        l_proj = delta_R @ l
-        delta_R_gyro = axis_angle_to_matrix(l_proj, delta_theta)
-        delta_R = delta_R_gyro @ delta_R
-    return delta_R
 
 # Multiply all gyro_delta_Rs together and return
 def integrate_gyro_delta_Rs(gyro_delta_Rs):
@@ -143,14 +126,26 @@ def matrix_to_axis_angle(A):
     axis = axis / la.norm(axis)
     return (axis,angle)
 
-def get_step_length_from_angle(angle):
-    return 0.762*angle
+# Use both angle and time to inform step length
+def get_step_length(angle, time):
+    time = time/1000
+    return (0.762*angle) +(0.25/(time**2))
 
-def get_step_length_from_angle_2(angle):
+# If person walking faster than 1 step every 0.5s
+# Then their stride length is at least a meter
+# Step time may inversely affect length
+def get_step_length_1(time):
+    time = time/1000
+    return (0.25/time**2)
+
+# EAAR approach: get dh of head, use that to inform step length
+# Maybe not applicable since IMU not on head
+def get_step_length_2(angle):
     delta_h = 0.8636 - ((0.8636)*np.cos(angle))
     step_length = 2 * delta_h * (np.sin(angle)/(1-np.cos(angle)))
     return step_length
 
+# Used to round each timestamp down to the nearest 0.01s
 def truncate(n, decimals=0):
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
@@ -166,6 +161,7 @@ def get_perpendicular(v):
     rotated_v = rotation.apply(v)
     return rotated_v
 
+# Project vector onto horizontal plane
 def project_onto_hor(v):
     grav = np.array([0,0,-1])
     proj_onto_grav = (np.dot(v, grav)/(la.norm(grav))) * grav
@@ -173,6 +169,7 @@ def project_onto_hor(v):
     proj_onto_hor = proj_onto_hor / la.norm(proj_onto_hor)
     return proj_onto_hor
 
+# Project all acc. data into global frame
 def get_acc_data_global(acc_data, all_Rs):
     acc_data_global = []
     for i in range(len(all_Rs)):
@@ -183,11 +180,13 @@ def get_acc_data_global(acc_data, all_Rs):
         acc_data_global.append([time, current_acc_g[0], current_acc_g[1], current_acc_g[2]])
     return acc_data_global
 
+# Convert to / from timestamp / index
 def ts_to_index(timestamp):
     return int(timestamp/10)
 def index_to_ts(index):
     return float(index*10)
 
+# Find outliers in dataset using std. dev.
 def find_outliers(data):
     anomalies = []
     # Set upper and lower limit to 3 standard deviation
@@ -202,15 +201,16 @@ def find_outliers(data):
             anomalies.append(outlier)
     return anomalies
 
+# Get nth maximum value of data, removing outliers first
 def get_nth_max_val(n, data):
+    # Gets nth maximum of data with outliers REMOVED
     outliers = find_outliers(data)
-    # print(outliers)
     data_no_outliers = list(filter(lambda d: not(d in outliers), data))
     data_no_outliers.sort()
     nth_max = data_no_outliers[len(data_no_outliers) - n]
-    # print(nth_max)
     return nth_max
 
+# Get statistical upper limit of data using std. dev., mean
 def get_upper_limit(data):
     data_std = np.std(data)
     data_mean = np.mean(data)
@@ -219,33 +219,94 @@ def get_upper_limit(data):
     upper_limit = data_mean + anomaly_cut_off
     return upper_limit
 
-def find_step_indices(data):
-    filtered_data = low_pass_filter(data)
+# (Not used anymore)
+# Moving average of data, attempting to smooth
+def moving_average(data):
+    N = 100
+    cumsum, moving_aves = [0], []
+    for i, x in enumerate(data, 1):
+        cumsum.append(cumsum[i-1] + x)
+        if i>=N:
+            moving_ave = (cumsum[i] - cumsum[i-N])/N
+            moving_aves.append(moving_ave)
+    return moving_aves
 
+# Detect step indices of filtered data
+def find_step_indices(filtered_data):
     # Find peaks of initial data, with no parameters
     (step_indices_temp, props) = find_peaks(filtered_data)
     peaks_temp = [filtered_data[i] for i in step_indices_temp]
-    # Get 5th peak
-    nth_max = get_nth_max_val(5, peaks_temp)
 
+    # Get 5th peak
+    # We will require minimum peak height to be some fraction of this 5th peak's height
+    # (Optionally) require a maximum peak height (calculated using std. dev)
+    nth_max = get_nth_max_val(5, peaks_temp)
     min_height = 0.75*nth_max
     max_height = get_upper_limit(filtered_data)
-
+    # Search for peaks again with specified params
     (step_indices, props) = find_peaks(filtered_data,
                                        threshold=peak_threshold,
                                        prominence=peak_prominence,
                                        distance=peak_distance,
                                        height=[min_height])
-    return (step_indices, filtered_data)
+    return step_indices
 
-# Plotting all 4 routes side-by-side
-if (PLOT_ROUTES):
+def plot_route_separate(locs, plot_index, route):
+    locs = np.array(locs)
+    plt.axis('equal')
+    p = plt.subplot(plot_index)
+    plt.plot(locs[:,0], locs[:,1])
+    plt.plot(0, 0, 'gs')
+    # p.set_xlabel('X position (m)')
+    # p.set_ylabel('Y position (m)')
+    plt.title('Route ' + route)
+
+def plot_route_same(locs, plot_index, route):
+    locs = np.array(locs)
+    plot_color = PLOT_COLORS[plot_index]
+    plt.plot(locs[:,0], locs[:,1], color=plot_color)
+    return
+
+def write_trajectory_to_file(locs, index, route_name):
+    path = './testing/'
+    path = './training/'
+    file_name = route_name + '.txt'
+    total_path = path + file_name
+    f = open(total_path, 'w+')
+    for loc in locs:
+        f.write(str(loc[0]) + ' ' + str(loc[1]) + '\n')
+    f.close()
+
+# -------- MAIN CODE --------
+routes = []
+if TESTING:
+    routes = ['1', '2', '3', '4', '5', '6']
+# TRAINING
+else:
+    routes = ['1', '2', '3', '4']
+
+plot_index = 0
+if PLOT_ROUTES:
     plt.figure(figsize=(16,9))
-plot_index = 241
-for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
+    # Plotting all routes in same graph
+    if PLOT_ROUTES_SAME:
+        plt.axis('equal')
+        plt.title('All Route Trajectories')
+    # Plotting all routes side-by-side
+    else:
+        plot_index = 221
 
-    acc_filename = './data_imu_loc/route' + route + '/Accelerometer.csv'
-    gyro_filename = './data_imu_loc/route' + route + '/Gyroscope.csv'
+index = 0
+for route in routes:
+    acc_filename, gyro_filename = '', ''
+
+    if TESTING:
+        acc_filename = './data_imu_loc_testing/route' + route + '/Accelerometer.csv'
+        gyro_filename = './data_imu_loc_testing/route' + route + '/Gyroscope.csv'
+    # TRAINING
+    else:
+        acc_filename = './data_imu_loc/route' + route + '/Accelerometer.csv'
+        gyro_filename = './data_imu_loc/route' + route + '/Gyroscope.csv'
 
     # ---- Read in data ----
     acc_data = read_data(acc_filename)
@@ -263,23 +324,6 @@ for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
 
     print(route)
 
-    # TODO: Dynamically set this cutoff based on actual data / peaks (?)
-    cutoff = 0
-    if (route == '1'):
-        cutoff = 100
-    if (route == '2'):
-        cutoff = 300
-    if (route == '3'):
-        cutoff = 85
-    if (route == '4'):
-        cutoff = 100
-    cutoff = 0
-
-    # Discard first datapoints since they may not be steps
-    acc_data = acc_data[cutoff:]
-    gyro_data = gyro_data[cutoff:]
-    all_Rs = all_Rs[cutoff:]
-
     # Round times to nearest 0.01 s (regard as constant 100Hz)
     acc_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in acc_data]
     gyro_data = [[truncate(row[0], 1), row[1], row[2], row[3]] for row in gyro_data]
@@ -292,7 +336,8 @@ for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
     # acc_data_global_zs = [(z - z_avg) for z in acc_data_global_zs]
 
     # ---- Filter & count steps from acc_data----
-    (step_indices, filtered_data) = find_step_indices(acc_data_global_zs)
+    filtered_data = low_pass_filter(acc_data_global_zs)
+    step_indices = find_step_indices(filtered_data)
     step_times = [acc_times[i] for i in step_indices]
     peaks = [filtered_data[i] for i in step_indices]
 
@@ -300,10 +345,12 @@ for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
 
     if not(PLOT_ROUTES):
         plt.figure(figsize=(16,9))
-        plt.plot(filtered_data[:5000], 'g')
-        step_ind_filt = list(filter(lambda x: x < 5000, step_indices))
-        peaks_filt = [filtered_data[i] for i in step_ind_filt]
-        plt.plot(step_ind_filt, peaks_filt, '.')
+        # plt.plot(filtered_data[:5000], 'g')
+        plt.plot(filtered_data, 'g')
+        # step_ind_filt = list(filter(lambda x: x < 5000, step_indices))
+        step_ind_filt = step_indices
+        # peaks_filt = [filtered_data[i] for i in step_ind_filt]
+        # plt.plot(step_ind_filt, peaks_filt, '.')
         plt.title('Path ' + route)
         plt.show()
 
@@ -311,6 +358,9 @@ for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
     locs = []
     locs.append(np.array([0,0,0]))
     even = True
+
+    times_between_steps = []
+
     for i in range(len(step_times) - 1):
         gyro_delta_Rs = get_gyro_delta_Rs_between_times(step_times[i], step_times[i+1], all_delta_Rs)
         delta_R_for_step = integrate_gyro_delta_Rs(gyro_delta_Rs)
@@ -319,26 +369,32 @@ for route in ['1', '2', '3', '4', 'Zach1', 'Zach2', 'Frank1', 'Frank2']:
         axis = project_onto_hor(axis)
         axis = get_perpendicular(axis)
 
+        # Flip axis on alternating steps
         if (even):
             axis = np.array([-1*axis[0], -1*axis[1], axis[2]])
 
         walking_dir = axis
-        step_length = get_step_length_from_angle_2(angle)
-        # step_length = 1
+        time_between_steps = step_times[i+1] - step_times[i]
+        step_length = get_step_length_1(time_between_steps)
         disp_of_step = step_length * walking_dir
         locs.append(locs[i] + disp_of_step)
 
+        times_between_steps.append(time_between_steps)
+
         even = not(even)
 
-    # Plot all 4 routes side-by-side (as 4 subplots)
+    print(np.mean(times_between_steps[:20])/1000)
 
-    if (PLOT_ROUTES):
-        locs = np.array(locs)
-        plt.axis('equal')
-        plt.subplot(plot_index)
-        plt.plot(locs[:,0], locs[:,1])
-        plt.plot(locs[len(locs)-1,0],locs[len(locs)-1,1], 'gs')
-        plt.title('Route ' + route)
+    write_trajectory_to_file(locs, index, route)
+
+    # Plot routes
+    if PLOT_ROUTES:
+        if PLOT_ROUTES_SAME:
+            plot_route_same(locs, plot_index, route)
+        else:
+            plot_route_separate(locs, plot_index, route)
         plot_index += 1
+
+    index += 1
 
 plt.show()
